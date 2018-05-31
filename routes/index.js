b6af8fd55045235343b1c0f81a8c963b7ea618de
RJ Simpson <silver911r@me.com>
2018-05-31 16:35:26 -0400

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('search');
+});
+
+router.get('/results', (req, res, next) => {
+  var search = req.query.search;
+  var url = `http://www.omdbapi.com/?apikey=${process.env.MOVIE_API_KEY}&s=` + search;
+  request(url, (error, response, body) => {
+
+
+
+    var data = JSON.parse(body);
+    console.log(data)
+    res.render('results', {data});
+  });
+});
+
+module.exports = router;
